7d78e9fedf8df8a40572c2233df114cb3fad0f0d
Abel Bolanos Martinez <martineza20@autonation.com>
2018-09-27 10:50:56 -0400

    Main function after generate the Reference.cs file

---
+++ mageSoap/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using mageSoap.ServiceReference;
+
+namespace mageSoap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mage_Api_Model_Server_Wsi_HandlerPortTypeClient client = new Mage_Api_Model_Server_Wsi_HandlerPortTypeClient();
+
+            //log in Magento API to get the session_id
+            loginResponse session = client.loginAsync("userAPI","keyAPI").Result;
+            string sessionId = session.result;
+            Console.WriteLine("Session ID: " + sessionId);
+
+            //Let's get the info from Order "100000010"
+            var response = client.salesOrderInfoAsync(sessionId,"100000010").Result;
+            salesOrderEntity order = response.result;
+            Console.WriteLine("Status in Order: " + order.status);
+        }
+    }
+}
